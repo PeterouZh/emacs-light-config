4571bf9a10aaaf5ad591d9141c799e4b022f2d2a
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2018-08-14 13:38:23 +0800

    fix highlight symbol for magit.
    	modified:   lisp/init-highlight-symbol.el

--- lisp/init-highlight-symbol.el
+++ lisp/init-highlight-symbol.el
@@ -4,4 +4,9 @@
 
 (peng-global-highlight-symbol-mode 1)
 
+(defun remove-highlight-symbol-mode ()
+  (peng-global-highlight-symbol-mode -1))
+
+(add-hook 'magit-mode-hook 'remove-highlight-symbol-mode)
+
 (provide 'init-highlight-symbol)
