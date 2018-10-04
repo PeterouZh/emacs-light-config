ff48d4b8a92be860fddb6f8b85af0e19f4eb69b5
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2018-10-04 08:49:30 +0800

    modified:   docker/docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-# sudo docker run --runtime=nvidia -it --name=tf_1_11 tensorflow/tensorflow:1.11.0-gpu-py3 /bin/bash 
+# Download tensorflow docker
+# sudo docker run --runtime=nvidia -it tensorflow/tensorflow:1.11.0-gpu-py3 /bin/bash 
 
-# copy files from docker container to host
+# Copy files from docker container to host
 # docker container cp 7b1d9d:/usr/lib/x86_64-linux-gnu/libcudnn.so.7.2.1 .
 
-# launch jupyter notebook in docker container
+# Launch jupyter notebook in docker container
 # sudo docker run --runtime=nvidia -it -p 8888:8888 tensorflow/tensorflow:1.11.0-gpu-py3
